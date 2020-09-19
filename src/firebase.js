23467631ae5df02b2b7bcb3c8b9c087ba4f6e571
devleopar <oktarkara@gmail.com>
2020-09-20 00:35:53 +0300

    initial stable version

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBP7aXjbMezxruVh9VaraouyqD381K9wSk",
+    authDomain: "challange-7c2c5.firebaseapp.com",
+    databaseURL: "https://challange-7c2c5.firebaseio.com",
+    projectId: "challange-7c2c5",
+    storageBucket: "challange-7c2c5.appspot.com",
+    messagingSenderId: "898373388687",
+    appId: "1:898373388687:web:1293ca1cade926a162a3fa",
+    measurementId: "G-55X8SCRFST"
+  };
